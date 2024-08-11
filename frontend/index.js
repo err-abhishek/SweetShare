5527539686868fd9e2c8d1d33ce0c541843b7e50
Abhishek Agrahari <abhishek737333@gmail.com>
2024-08-11 12:34:20 +0530

    url changed

--- frontend/index.js
+++ frontend/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://sweetsharebackend-abhisheks-projects-bda1edf4.vercel.app";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
